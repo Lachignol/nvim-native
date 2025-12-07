3e2d3788dcb5edce2b7a41f53e5fc2349bde9bb5
Votre Nom <votre_email@exemple.com>
2025-12-07 23:09:40 +0100

    grep by default live

--- .config/nvim/lua/plugins/plugin_configs/mini_pick.lua
+++ .config/nvim/lua/plugins/plugin_configs/mini_pick.lua
@@ -11,5 +11,5 @@ require("mini.pick").setup({
 -- Keymaps pour ouvrir les pickers mini.pick intégrés
 vim.keymap.set('n', '<leader><leader>', ":Pick files<CR>", { desc = "Ouvrir picker fichiers" })
 vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>", { desc = "Ouvrir picker buffers" })
-vim.keymap.set('n', '<leader>g', ":Pick grep<CR>", { desc = "Lancer recherche grep" })
+vim.keymap.set('n', '<leader>g', ":Pick grep_live<CR>", { desc = "Lancer recherche grep" })
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>", { desc = "Ouvrir picker help" })

6ebe450e2c12aff054c94fea7fc35886ca111591
Votre Nom <votre_email@exemple.com>
2025-12-08 03:54:50 +0100

    add clue and cleaning

--- .config/nvim/lua/plugins/plugin_configs/mini_pick.lua
+++ .config/nvim/lua/plugins/plugin_configs/mini_pick.lua
@@ -11,6 +11,5 @@ require("mini.pick").setup({
 })
 -- Keymaps pour ouvrir les pickers mini.pick intégrés
 vim.keymap.set('n', '<leader><leader>', ":Pick files<CR>", { desc = "Chercher fichier" })
-vim.keymap.set('n', '<leader>b', ":Pick buffers<CR>", { desc = "Chercher buffers" })
 vim.keymap.set('n', '<leader>g', ":Pick grep_live<CR>", { desc = "Chercher grep" })
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>", { desc = "Chercher help" })
